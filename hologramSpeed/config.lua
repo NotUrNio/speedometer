c96cba06caaee181aab4ab9346527e3ed2ca82d6
NotUrNio <niowork477@gmail.com>
2024-10-15 08:42:14 +0530

    Add files via upload

---
+++ hologramSpeed/config.lua
@@ -0,0 +1,10 @@
+Config = {
+	-- The hologram model name, do not change this
+	modelName = 'hologram_box_model',
+	-- The speedometer UI page url
+	duiUrl = 'https://github.com/NotUrNio/speedometer-fivem/blob/main/hologram.html',
+	-- User default profile --
+	keyControl = 243,   -- The key to enable or disable the speedometer
+	useMph     = true,  -- Using MPH unit
+	useNve     = false, -- Using NVE graphic mods
+}
